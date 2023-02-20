94ac746cdd18ab72926952dd68be37a0b78cd33c
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-02-20 14:29:06 +0600

    Adding comments to app.js

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+// Initializing the app:
 const express = require('express');
-
 const app = express();
 
+// Setting view engine to pug:
 app.set('view engine', 'pug');
 
+// Setting app routes:
+app.get('/', (req, res) => {
+    res.send()
+});
+
+// Starting server, app is listening on port 3000 and logs the port at the terminal console( with nodeman):
 app.listen(3000, () => {
     console.log('The app is running at localhost 3000');
 });
