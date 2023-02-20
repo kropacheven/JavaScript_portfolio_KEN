6658eaa3151c95fa608ef791315e439aa2cec205
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-02-20 14:34:40 +0600

    Launch of server is made available

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-// Initializing the app:
+// 1. Initializing the app:
 const express = require('express');
 const app = express();
 
-// Setting view engine to pug:
+// 2. Setting view engine to pug:
 app.set('view engine', 'pug');
 
-// Setting app routes:
+// 2. Setting app routes
+
+// a. Main page route:
 app.get('/', (req, res) => {
-    res.send()
+    res.send('<h1>This is the main page of the app.</h1>')
 });
 
-// Starting server, app is listening on port 3000 and logs the port at the terminal console( with nodeman):
+// b. About page route:
+app.get('/about', (req, res) => {
+    res.send('<h2>This is the about page of the app.</h2>')
+});
+
+// c. Dynamic projects routes:
+// (based on the id of the project that render a customized version of the Pug project template to show off each project):
+
+
+
+// 3. Starting server, app is listening on port 3000 and logs the port at the terminal console( with nodeman):
 app.listen(3000, () => {
     console.log('The app is running at localhost 3000');
 });
