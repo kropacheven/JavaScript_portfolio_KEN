6366847f9d1059744b99396b3ff8e84fb4f9150d
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-02-20 14:23:12 +0600

    Initialising app at app.js
    First steps

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.listen(3000, () => {
+    console.log('The app is running at localhost 3000');
+});
